f5dfd66eddf1aa2fa8c6576f34893225b195f02d
Alox1d <alox1dab@gmail.com>
2023-12-24 13:36:31 +0300

    custom coroutineScope: CoroutineContext consists of 4 items - dispatchers, Job, ExceptionHandler, CoroutineName

--- app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
@@ -1,20 +1,24 @@
 package com.sumin.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
-import kotlin.concurrent.thread
-import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State> = _state
+
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.Main + CoroutineName("My Coroutine Name"))
 
     /*
     private val _error = MutableLiveData<Boolean>()
@@ -27,44 +31,50 @@ class MainViewModel : ViewModel() {
      val progress: LiveData<Boolean> = _progress
      */
 
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
     fun calculate(value: String?) {
         _state.value = Progress
         if (value.isNullOrBlank()) {
             _state.value = Error
             return
         }
-        val number = value.toLong()
 
         // calculate
-        viewModelScope.launch {
-            val result = factorial(number)
-            _state.value = Factorial(value = result.toString())
+        coroutineScope.launch {
+            val number = value.toLong()
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
+            _state.value = (Factorial(value = result))
+            Log.d("VM", "calculate: $coroutineContext")
         }
     }
 
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
+        }
+        return result.toString()
+    }
+
+
+    /*
     private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
+    return suspendCoroutine {
+        thread {
             var result = BigInteger.ONE
             for (i in 1..number) {
                 result = result.multiply(BigInteger.valueOf(i))
             }
-            result.toString()
+            it.resumeWith(Result.success(result.toString())) // Result.failure - to throw exception
+            // return result.toString()
         }
     }
-
-    /*
-    private suspend fun factorial(number: Long): String {
-        return suspendCoroutine {
-            thread {
-                var result = BigInteger.ONE
-                for (i in 1..number) {
-                    result = result.multiply(BigInteger.valueOf(i))
-                }
-                it.resumeWith(Result.success(result.toString())) // Result.failure - to throw exception
-                // return result.toString()
-            }
-        }
-    }
-
-     */
+}
+ */
 }
