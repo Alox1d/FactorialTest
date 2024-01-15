5a21e5005ccb0e4167bc52f4201bd28dbbe5abd9
Alox1d <alox1dab@gmail.com>
2024-01-15 22:29:13 +0300

    custom CoroutineScope

--- app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/sumin/factorialtest/MainViewModel.kt
@@ -1,15 +1,20 @@
 package com.sumin.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default + CoroutineName("My Coroutina"))
 
     private val _state = MutableLiveData<UiState>()
     val state: LiveData<UiState> = _state
@@ -20,12 +25,13 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        coroutineScope.launch(Dispatchers.Main) {
             val number = value.toLong()
             val result = withContext(Dispatchers.Default) {
                 factorial(number)
             }
             _state.value = Result(factorial = result)
+            Log.d("MainViewModel", "calculate: $coroutineContext")
         }
     }
 
@@ -35,5 +41,10 @@ class MainViewModel : ViewModel() {
             result = result.multiply(BigInteger.valueOf(i))
         }
         return result.toString()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
     }
 }
